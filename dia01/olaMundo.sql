f74f4d7129f74891348f67a2ed3cf0cbe9e2b707
suiciniv-s@hotmail.com <suiciniv-s@hotmail.com>
2023-05-24 23:48:52 +0000

    Primeiro dia
    Hoje conectamos o github no Databricks e fizemos uma consulta no banco de dados

---
+++ dia01/olaMundo.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+SELECT 'Olá Mundo!'
+
+-- COMMAND ----------
+
+SELECT *                        -- selecione todas as colunas
+FROM silver.pizza_query.pedido  -- da tabela silver.pizzaquery.pedido
+
+-- COMMAND ----------
+
+
